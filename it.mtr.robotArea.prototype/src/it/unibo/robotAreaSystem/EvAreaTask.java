4fcc885054d2b1e258d8d9526d415f7dd2bc1214
Pierluigi Montagna <pierluigi.montagna@studio.unibo.it>
2015-01-21 15:04:43 +0100

    added base task

---
+++ it.mtr.robotArea.prototype/src/it/unibo/robotAreaSystem/EvAreaTask.java
@@ -0,0 +1,16 @@
+package it.unibo.robotAreaSystem;
+
+import it.unibo.contactEvent.interfaces.IEventItem;
+
+public class EvAreaTask extends EvAreaTaskSupport {
+
+	public EvAreaTask(String name, String[] eventId) throws Exception {
+		super(name, eventId);
+	}
+
+	@Override
+	protected void handleEvent(IEventItem curEvent) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ it.mtr.robotArea.prototype/src/it/unibo/robotAreaSystem/UserActor.java
@@ -0,0 +1,18 @@
+package it.unibo.robotAreaSystem;
+
+import it.unibo.is.interfaces.IOutputView;
+import it.unibo.qactors.ActorContext;
+import it.unibo.qactors.QActor;
+
+public class UserActor extends QActor {
+
+	public UserActor(String actorId, ActorContext myCtx, IOutputView outView) {
+		super(actorId, myCtx, outView);
+	}
+
+	@Override
+	protected void doJob() throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+}
