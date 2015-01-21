1e9717cd11d90525666d3d69b1da07ef991f174d
Pierluigi Montagna <pierluigi.montagna@studio.unibo.it>
2015-01-21 15:35:59 +0100

    .

--- it.mtr.robotArea.prototype/src/it/unibo/robotAreaSystem/UserActor.java
+++ it.mtr.robotArea.prototype/src/it/unibo/robotAreaSystem/UserActor.java
@@ -1,5 +1,10 @@
 package it.unibo.robotAreaSystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import it.unibo.contactEvent.platform.ContactEventPlatform;
 import it.unibo.is.interfaces.IOutputView;
 import it.unibo.qactors.ActorContext;
 import it.unibo.qactors.QActor;
@@ -12,7 +17,47 @@ public class UserActor extends QActor {
 
 	@Override
 	protected void doJob() throws Exception {
-		// TODO Auto-generated method stub
+		String cmd = "";
+		while (!cmd.equals("e") && !cmd.equals("q")) {
+			cmd = waitForUserCommand();
+			executeCmd(cmd);
+		}
+		outView.addOutput("bye bye");
+	}
+
+	private void executeCmd(String cmd) {
+		switch (cmd) {
+		case "halt":
+			emitEvent("halt", "robotArea");
+			break;
+		case "ld":
+			emitEvent("lineDetected", "robotArea");
+			break;
+		case "lud":
+			emitEvent("lineUndetected", "robotArea");
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	private void emitEvent(String evId, String evContent){
+		println(getName() + " emits " + evId + " " + evContent);
+		this.platform.raiseEvent(getName(), evId, evContent);
+	}
+
+	private String waitForUserCommand() {
+		outView.addOutput("cmd: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+		try {
+			line = br.readLine();
+		} catch (IOException e) {
+			println("Error " + e.getMessage());
+			e.printStackTrace();
+		}
+		return line;
 	}
 
 }
